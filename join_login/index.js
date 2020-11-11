eec5f87b2a52dacb0d7750437608c7f625fe5eeb
ddock4you <ddock4you@gmail.com>
2020-11-11 22:46:39 +0900

    - auth 기능 추가

--- join_login/index.js
+++ join_login/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/user");
 
-const app = express();
 mongoose
     .connect(
         "mongodb+srv://ddock4you:tmdgus123!@cluster0.wlayg.mongodb.net/boilerplate?retryWrites=true&w=majority",
@@ -20,11 +23,11 @@ const PORT = 5001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("홈 화면"));
 app.post("/register", (req, res) => {
     const user = new User(req.body);
-
     user.save((err) => {
         if (err)
             return res.json({
@@ -35,6 +38,34 @@ app.post("/register", (req, res) => {
         return res.status(200).json({
             joinSuccess: true,
         });
+    });
+});
+app.post("/login", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) return res.json({ loginSuccess: false });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({ loginSuccess: false });
+
+            user.generateToken((err, user) => {
+                if (err) return res.json({ loginSuccess: false, err });
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true });
+            });
+        });
+    });
+});
+
+app.get("/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        lastname: req.user.lastemail,
+        role: req.user.role,
+        image: req.user.image,
     });
 });
 
--- join_login/models/user.js
+++ join_login/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const saltRounds = 10;
 
 const userSchema = new Schema({
@@ -32,20 +34,55 @@ const userSchema = new Schema({
 
 userSchema.pre("save", function (next) {
     const user = this;
-
     if (user.isModified("password")) {
-        bcrypt.genSalt(saltsRounds, function (err, salt) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err);
 
             bcrypt.hash(user.password, salt, function (err, hash) {
                 if (err) return next(err);
                 user.password = hash;
+                next();
             });
         });
+    } else {
+        next();
     }
-
-    next();
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, result) {
+        if (err) return cb(err);
+        cb(null, result);
+    });
+};
+
+userSchema.methods.generateToken = function (cb) {
+    const user = this;
+
+    const token = jwt.sign(user._id.toString(), "token");
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+    const user = this;
+
+    jwt.verify(token, "token", function (err, decoded) {
+        user.findOne(
+            {
+                _id: decoded,
+                token: token,
+            },
+            function (err, user) {
+                if (err) return cb(err);
+                cb(null, user);
+            }
+        );
+    });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = {
