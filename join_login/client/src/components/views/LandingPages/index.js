dd4af5fc8441a2e7019a17bad7533788329c9d41
ddock4you <ddock4you@gmail.com>
2020-11-18 00:12:53 +0900

    - CORS 해결

--- join_login/client/src/components/views/LandingPages/index.js
+++ join_login/client/src/components/views/LandingPages/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const LandingPage = () => {
     useEffect(() => {
-        // axios.get("/hello").then((res) => console.log(res));
+        axios.get("/api/hello").then((res) => console.log(res));
     }, []);
 
     return <div>LandingPage</div>;
--- join_login/client/src/setupProxy.js
+++ join_login/client/src/setupProxy.js
@@ -2,8 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware("/", {
-            target: "http://localhost:5001/",
+        "/api",
+        createProxyMiddleware({
+            target: "http://localhost:5001",
             changeOrigin: true,
         })
     );
